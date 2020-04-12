924be792c952010e0c8455a1eb98e563e8e2b04c
luzj <luzj@hoolai.com>
2020-04-12 15:02:22 +0800

    用synthic替代单独的实现

--- src/main/java/dev/xframe/boot/ShutdownAgent.java
+++ src/main/java/dev/xframe/boot/ShutdownAgent.java
@@ -1,5 +1,8 @@
 package dev.xframe.boot;
 
+import dev.xframe.inject.Synthetic;
+
+@Synthetic
 public interface ShutdownAgent {
 	
 	public void shutdown();
--- src/main/java/dev/xframe/boot/ShutdownHook.java
+++ src/main/java/dev/xframe/boot/ShutdownHook.java
@@ -1,20 +1,18 @@
 package dev.xframe.boot;
 
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
 
-import dev.xframe.inject.ApplicationContext;
 import dev.xframe.inject.Configurator;
 import dev.xframe.inject.Eventual;
-import dev.xframe.inject.Ordered;
-import dev.xframe.inject.Ordered.Collection;
-import dev.xframe.inject.code.Codes;
+import dev.xframe.inject.Inject;
+import dev.xframe.inject.code.SyntheticBuilder;
 import dev.xframe.utils.XLogger;
 
 @Configurator
 public class ShutdownHook implements Eventual {
 
-	private Collection<ShutdownAgent> agents;
+	@Inject
+	private ShutdownAgent agents;
 	
 	private Thread hook = new Thread(this::shutdownNow, "hook");
 	
@@ -37,14 +35,14 @@ public class ShutdownHook implements Eventual {
 				XLogger.info("Shutting down net servers");
 			}
 			
-			for (ShutdownAgent agent : agents) {
+			SyntheticBuilder.forEach(agents, (ShutdownAgent agent)->{
 				try {
 					agent.shutdown();
 					XLogger.info("Shutting down agent [" + agent.getClass().getSimpleName() + "]");
 				} catch (Throwable e) {
 					XLogger.warn("Shutting down agent", e);
 				}
-			}
+			});
 			
 			try {
                 Runtime.getRuntime().removeShutdownHook(hook);
@@ -62,25 +60,7 @@ public class ShutdownHook implements Eventual {
 
 	@Override
 	public void eventuate() {
-		agents = Codes.getDeclaredClasses().stream()
-					.filter(this::isAgent)
-					.filter(this::existsBean)
-					.map(this::getAgentBean)
-					.collect(Collectors.toCollection(Ordered.Collection::new));
-		
 		Runtime.getRuntime().addShutdownHook(this.hook);
-	}
-
-	private boolean isAgent(Class<?> c) {
-		return ShutdownAgent.class.isAssignableFrom(c) && !ShutdownAgent.class.equals(c);
-	}
-
-	private boolean existsBean(Class<?> c) {
-		return getAgentBean(c) != null;
-	}
-
-	private ShutdownAgent getAgentBean(Class<?> c) {
-		return (ShutdownAgent) ApplicationContext.fetchBean(c);
 	}
 
 }
