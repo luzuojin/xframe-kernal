920ee0dcbcd1687b0745a2d84e3325b3eccae152
luzj <luzj@hoolai.com>
2020-08-04 12:11:21 +0800

    refactor websocket message codec

--- src/main/java/dev/xframe/net/websocket/WebSocketMessageCodec.java
+++ src/main/java/dev/xframe/net/websocket/WebSocketMessageCodec.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import dev.xframe.net.codec.IMessage;
 import dev.xframe.net.codec.MessageCodec;
+import dev.xframe.utils.XProperties;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageCodec;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 
-public class WebSocketMessageCodec extends MessageToMessageCodec<BinaryWebSocketFrame, IMessage>{
+public class WebSocketMessageCodec extends MessageToMessageCodec<WebSocketFrame, IMessage>{
+    
+    private boolean outFrameUseBinary = XProperties.getAsBool("xframe.websocket.binary", false);
 	
 	private MessageCodec iCodec;
 	
@@ -18,11 +23,13 @@ public class WebSocketMessageCodec extends MessageToMessageCodec<BinaryWebSocket
 	}
 	
 	@Override
-	protected void decode(ChannelHandlerContext ctx, BinaryWebSocketFrame bwsf, List<Object> out) throws Exception {
-	    ByteBuf buf = bwsf.content();
-	    IMessage message = iCodec.decode(ctx, buf);
-	    if(message != null) {
-	        out.add(message);
+	protected void decode(ChannelHandlerContext ctx, WebSocketFrame bwsf, List<Object> out) throws Exception {
+	    if(bwsf instanceof TextWebSocketFrame || bwsf instanceof BinaryWebSocketFrame) {
+	        ByteBuf buf = bwsf.content();
+	        IMessage message = iCodec.decode(ctx, buf);
+	        if(message != null) {
+	            out.add(message);
+	        }
 	    }
 	}
 
@@ -30,7 +37,8 @@ public class WebSocketMessageCodec extends MessageToMessageCodec<BinaryWebSocket
 	protected void encode(ChannelHandlerContext ctx, IMessage message, List<Object> out) throws Exception {
 		ByteBuf buf = ctx.alloc().ioBuffer();
 		iCodec.encode(ctx, message, buf);
-        out.add(new BinaryWebSocketFrame(buf));
+		WebSocketFrame outFrame = outFrameUseBinary ? new BinaryWebSocketFrame(buf) : new TextWebSocketFrame(buf);
+        out.add(outFrame);
 	}
 
 
