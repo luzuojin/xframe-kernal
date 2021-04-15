03ae44bc25c1b088ee71c88a665fe43b40678f07
luzj <luzj@hulai.com>
2021-04-15 11:24:42 +0800

    lazy start ScheduledContext

--- src/main/java/dev/xframe/task/ScheduledContext.java
+++ src/main/java/dev/xframe/task/ScheduledContext.java
@@ -1,22 +1,33 @@
 package dev.xframe.task;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import dev.xframe.inject.Configurator;
 import dev.xframe.task.scheduled.ScheduledTask;
 import dev.xframe.utils.XProperties;
 
 @Configurator
 public class ScheduledContext {
-
-	private TaskExecutor executor = TaskExecutors.newFixed("scheduled", scheduledThreads());
 	
-	private TaskLoop loop = new TaskLoop.Direct(executor);
+    private AtomicInteger running = new AtomicInteger(0);
 
-	private int scheduledThreads() {
+	private TaskExecutor executor;
+	
+	private TaskLoop loop;
+	
+	private void ensure() {
+		if(running.get() == 0 && running.compareAndSet(0, 1)) {
+			executor = TaskExecutors.newFixed("scheduled", threads());
+			loop = new TaskLoop.Direct(executor);
+		}
+	}
+	private int threads() {
 		int def = Math.min(1, Runtime.getRuntime().availableProcessors() / 2);
 		return XProperties.getAsInt("xframe.scheduledthreads", def);
 	}
 	
 	public TaskLoop loop() {
+		this.ensure();
 		return loop;
 	}
 	
@@ -25,11 +36,11 @@ public class ScheduledContext {
 	}
 	
 	public void period(Runnable task, int period) {
-		ScheduledTask.period(loop, period, task).checkin();
+		ScheduledTask.period(loop(), period, task).checkin();
 	}
 	
 	public void period(Runnable task, int delay, int period) {
-		ScheduledTask.period(loop, delay, period, task).checkin();
+		ScheduledTask.period(loop(), delay, period, task).checkin();
 	}
 	
 }
