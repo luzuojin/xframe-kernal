f3f1b345e3ec2a2f0d94240d431045db6ee37777
luzj <luzj@hulai.com>
2022-12-29 14:56:31 +0800

    startup with properties file

--- src/main/java/dev/xframe/boot/Bootstrap.java
+++ src/main/java/dev/xframe/boot/Bootstrap.java
@@ -1,5 +1,9 @@
 package dev.xframe.boot;
 
+import dev.xframe.http.HttpServer;
+import dev.xframe.net.NetServer;
+import dev.xframe.net.WebSocketServer;
+import dev.xframe.utils.XProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -67,5 +71,46 @@ public class Bootstrap {
         }
         return this;
     }
-    
+
+    /**
+     * startup from xproperties
+     * @return
+     */
+    public static Bootstrap fromProperties() {
+        //basic keys
+        String kName    = "xframe.boot.name";
+        String kInclude = "xframe.boot.include";
+        String kExclude = "xframe.boot.exclude";
+        //http keys
+        String kHttpPort    = "xframe.boot.http.port";
+        String kHttpThreads = "xframe.boot.http.threads";
+        //tcp keys
+        String kTcpPort    = "xframe.boot.tcp.port";
+        String kTcpThreads = "xframe.boot.tcp.threads";
+        //ws keys
+        String kWsPort    = "xframe.boot.websocket.port";
+        String kWsHost    = "xframe.boot.websocket.host";
+        String kWsThreads = "xframe.boot.websocket.threads";
+
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.withName(XProperties.get(kName));
+        bootstrap.include(XProperties.get(kInclude));
+        bootstrap.exclude(XProperties.get(kExclude));
+        int httpPort = XProperties.getAsInt(kHttpPort);
+        if (httpPort > 0) {
+            bootstrap.withHttp(httpPort, XProperties.getAsInt(kHttpThreads, HttpServer.defaultThreads()));
+        }
+        int tcpPort = XProperties.getAsInt(kTcpPort);
+        if (tcpPort > 0) {
+            bootstrap.withHttp(tcpPort, XProperties.getAsInt(kTcpThreads, NetServer.defaultThreads()));
+        }
+        int wsPort = XProperties.getAsInt(kWsPort);
+        if (wsPort > 0) {
+            bootstrap.withWebSocket(XProperties.get(kWsHost), wsPort, XProperties.getAsInt(kWsThreads, WebSocketServer.defaultThreads()));
+        }
+        return bootstrap;
+    }
+    public static void main(String[] args) {
+        fromProperties().startup();
+    }
 }
